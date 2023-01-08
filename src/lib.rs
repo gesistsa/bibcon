12264e5800b5446a6cc10f6c1f51c2bfa629dec4
chainsawriot <chainsawtiney@gmail.com>
2023-01-08 16:39:41 +0100

    add a minimal viable product

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use lazy_static::lazy_static;
-
 use regex::Regex;
 use std::collections::HashSet;
+use std::fs;
+
+use biblatex::Bibliography;
 
 pub fn extract_citekeys(text: &str) -> HashSet<&str>{
     lazy_static! {
@@ -12,12 +14,34 @@ pub fn extract_citekeys(text: &str) -> HashSet<&str>{
     HASHTAG_REGEX.captures_iter(text).map(|c| c.name("key").unwrap().as_str()).collect()
 }
 
+fn read_bibtex(path: &str) -> Bibliography {
+    let content = fs::read_to_string(&path).expect("Should have been able to read the file");
+    Bibliography::parse(&content).expect("Unable to parse the main BibTeX file.")
+}
+
+fn extract_bibtex(keys: &HashSet<&str>, bib: &Bibliography) -> Bibliography {
+    let mut condensed_bib = Bibliography::new();
+    for key in keys {
+	condensed_bib.insert(Clone::clone(bib.get(key).expect("key not found!")));
+    };
+    condensed_bib
+}
+
+pub fn bibcon(md_path: &str, bib_path: &str) {
+    let text = fs::read_to_string(md_path)
+	.expect("Should have been able to read the file");
+    let keys = extract_citekeys(&text);
+    let bib = read_bibtex(bib_path);
+    let cbib = extract_bibtex(&keys, &bib);
+    println!("{}", cbib.to_bibtex_string());
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fs;
     #[test]
-    fn extract_citekeys_basic() {
+    fn ut_extract_citekeys_basic() {
 	let text = "[@abc;@efg p. 1234] chung-hong.chan@gesis.org @hij \\@ref(fig:fig5)";
 	let keys = extract_citekeys(&text);
 	assert_eq!(keys.len(), 3);
@@ -27,7 +51,7 @@ mod tests {
 	assert_eq!(keys.contains("hij"), true);
     }
     #[test]
-    fn extract_citekeys_real1() {
+    fn ut_extract_citekeys_real1() {
 	let text = fs::read_to_string("tests/weat.qmd")
 	    .expect("Should have been able to read the file");
 	let keys = extract_citekeys(&text);
@@ -38,5 +62,20 @@ mod tests {
 	assert_eq!(keys.contains("garg:2018:W"), true);
 	assert_eq!(keys.contains("chan:2022"), true);
 	assert_eq!(keys.contains("gesis"), false);
+    }
+    #[test]
+    fn ut_read_bibtext() {
+	let bib = read_bibtex("tests/weat.bib");
+	assert_eq!(bib.len(), 6);
+    }
+    #[test]
+    fn ut_extract_bibtex() {
+	let text = fs::read_to_string("tests/weat.qmd")
+	    .expect("Should have been able to read the file");
+	let keys = extract_citekeys(&text);
+	let bib = read_bibtex("tests/weat.bib");
+	let cbib = extract_bibtex(&keys, &bib);
+	assert_eq!(bib.len(), 6);
+	assert_eq!(cbib.len(), 5);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let md_filename = &args[1];
-    let text = fs::read_to_string(md_filename)
-        .expect("Should have been able to read the file");
-    let keys = bibcon::extract_citekeys(&text);
-    for key in &keys {
-	println!("key: {key}");
-    }
+    let bib_filename = &args[2];
+    bibcon::bibcon(md_filename, bib_filename);
 }
