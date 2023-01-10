6ec1b7fec569cfd1dc35b79f9c239a987d6d4b89
chainsawriot <chainsawtiney@gmail.com>
2023-01-10 11:39:32 +0100

    make MDFILES a required field

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use clap::{Arg, App};
 fn main() {
     let matches = App::new("BibTeX Condenser").
 	version("0.0.1").
-	author("Chung-hong Chan").
+	author("Chung-hong Chan <chung-hong.chan@gesis.org>").
 	arg(Arg::with_name("bib").short("b").long("bib").value_name("BIBFILE").help("Main BibTeX file").takes_value(true)).
-	arg(Arg::with_name("md").value_name("mdfiles").help("Multiple Markdown files").min_values(1)).
+	arg(Arg::with_name("md").value_name("MDFILES").help("Markdown file(s)").required(true).min_values(1)).
 	get_matches();
     let bib_filename = matches.value_of("bib").unwrap();
     let md_filename: Vec<_> = matches.values_of("md").unwrap().collect();
