b89b16ae9fab8f3c5b2421444e30bc62ec6bd000
chainsawriot <chainsawtiney@gmail.com>
2023-09-27 19:30:05 +0200

    Bump version

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use clap::{Arg, App};
 
 fn main() {
     let matches = App::new("BibTeX Condenser").
-	version("0.0.3").
-	author("Chung-hong Chan <chung-hong.chan@gesis.org>").
+	version("0.0.4").
+	author("Chung-hong Chan <chainsawtiney@gmail.com>").
 	arg(Arg::with_name("bib").short("b").long("bib").value_name("BIBFILE").help("Main BibTeX file").takes_value(true)).
 	arg(Arg::with_name("outbib").short("o").long("outbib").value_name("OUTBIBFILE").help("Output BibTeX file").takes_value(true)).
 	arg(Arg::with_name("md").value_name("MDFILES").help("Markdown file(s)").required(true).min_values(1)).
